7ec460a3a4463f4cd6d51504c1182149f5912cb5
Votre m <Vous@exemle.com>
2022-02-04 16:41:33 +0100

    second commit

--- routes/web.php
+++ routes/web.php
@@ -29,32 +29,47 @@ Route::get ('/accueil', [
 
 Route::get ('/admin_index/adminpc', [
     'as' => 'adminpc_path',
-    'uses' => 'App\Http\Controllers\FrontController@adminpc',
+    'uses' => 'App\Http\Controllers\FrontController@adminPc',
     ]);
 
 Route::get ('/admin_index/adminpc/edit', [
-   'as' => 'adminpc_path',
-    'uses' => 'App\Http\Controllers\FrontController@adminpc',
+   'as' => 'adminpc-edit_path',
+    'uses' => 'App\Http\Controllers\FrontController@adminPcEdit',
+    ]);
+
+Route::get ('/admin_index/adminpc/liste', [
+    'as' => 'adminpc-liste_path',
+    'uses' => 'App\Http\Controllers\FrontController@adminPcListe',
     ]);
 
 Route::get ('/admin_index/admincont', [
     'as' => 'admincont_path',
-    'uses' => 'App\Http\Controllers\FrontController@admincont',
+    'uses' => 'App\Http\Controllers\FrontController@adminCont',
+    ]);
+
+Route::get ('/admin_index/admincont/edit', [
+    'as' => 'admincont-edit_path',
+    'uses' => 'App\Http\Controllers\FrontController@adminContEdit',
+    ]);
+
+Route::get ('/admin_index/admincont/liste', [
+    'as' => 'admincont-liste_path',
+    'uses' => 'App\Http\Controllers\FrontController@adminContListe',
     ]);
 
 
-Route::get ('/gestion_index/leve', [
-    'as' => 'leve_path',
-    'uses' => 'App\Http\Controllers\FrontController@leve',
+Route::get ('/gestion_index/liste', [
+    'as' => 'gestion-liste_path',
+    'uses' => 'App\Http\Controllers\FrontController@gestionListe',
     ]);
 
 Route::get ('/gestion_index/rapport', [
     'as' => 'rapport_path',
-    'uses' => 'App\Http\Controllers\FrontController@rapport',
+    'uses' => 'App\Http\Controllers\FrontController@gestionRapport',
     ]);
 
-Route::get ('/gestion_index/pointcollecte', [
-    'as' => 'pointcollec_path',
-    'uses' => 'App\Http\Controllers\FrontController@pointcollec',
+Route::get ('/gestion_index/rapport/edit', [
+    'as' => 'rapport-edit_path',
+    'uses' => 'App\Http\Controllers\FrontController@gestionRapportEdit',
     ]);
 
